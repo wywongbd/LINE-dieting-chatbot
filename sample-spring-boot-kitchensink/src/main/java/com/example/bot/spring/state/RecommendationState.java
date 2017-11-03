f79e9cf2c52f97d08209c15354695e9d9c703da4
bachang96 <bachang96@hotmail.com>
2017-11-03 17:54:52 +0800

    Completed DB part of the recommendation system

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/state/RecommendationState.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/state/RecommendationState.java
@@ -1,10 +1,13 @@
 package com.example.bot.spring;
 
 //import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import com.rivescript.RiveScript;
 import com.example.bot.spring.SQLDatabaseEngine;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+@Slf4j
 public class RecommendationState extends State {
     /**
      * Default constructor for RecommendationState
@@ -34,10 +37,21 @@ public class RecommendationState extends State {
      * @param userID A int data type
      * @return A String data type
      */
-	public String recommendation(ArrayList<String> foodList, int userID) {
-		SQLDatabaseEngine db = new SQLDatabaseEngine();
+	public String recommendation(String userId, ArrayList<String> foodList) {
+		SQLDatabaseEngine sql = new SQLDatabaseEngine();
+		HashMap<String, Double> foodWeightage = null;
 		
-		
+		try {
+			sql.addMenu(userId, foodList);
+			sql.addRecommendations(userId);
+			sql.processRecommendationsByAllergies(userId);
+			sql.processRecommendationsByIntake(userId);
+			foodWeightage = sql.getRecommendations(userId);
+		} catch (Exception e) {
+			log.info("Exception while removing recommendations from recommendations table: {}", e.toString());
+			return "I am not sure what to recommend you";
+		}
+
 		
 		return "Food";
 	}
