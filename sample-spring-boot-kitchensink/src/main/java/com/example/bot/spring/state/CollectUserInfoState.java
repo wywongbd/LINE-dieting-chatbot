ff696301c74be959e4c029f07840ab1d3044a453
wywongbd <emarald10@gmail.com>
2017-11-18 13:38:23 +0800

    debugging

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/state/CollectUserInfoState.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/state/CollectUserInfoState.java
@@ -23,9 +23,13 @@ public class CollectUserInfoState extends State {
     }
 
 	public String reply(String userId, String text, RiveScript bot) {
+		System.out.println("CollectUserInfoState point 1");
+
 		String currentState = bot.getUservar(userId, "state"); 
 		String output = bot.reply(userId, text);
 		String afterState = bot.getUservar(userId, "state");
+
+		System.out.println("CollectUserInfoState point 2");
 				
 		if (currentState != afterState) {
 			// write to DB
@@ -54,8 +58,11 @@ public class CollectUserInfoState extends State {
 				System.out.println("Exception while inserting user info into user database: " + e.toString());
 			}
 		}
+		System.out.println("CollectUserInfoState point 3");
 
 		updateDatabase(userId, bot);
+
+		System.out.println("CollectUserInfoState point 4");
 		return output;
 	}
 }
